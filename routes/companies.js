7921e05b80725f2fd834ef43f3798b87df0ad22c
Chad Smith <chadsmithmusic@icloud.com>
2022-02-04 20:18:01 -0800

    .put route

--- routes/companies.js
+++ routes/companies.js
@@ -64,7 +64,16 @@ router.get("/:code", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
     try {
         let {name, description} = req.body;
-        let code = slugify
+        let code = slugify(name, {lower:true});
+
+        const result = await db.query(
+            `INSERT INTO companies (code, name, description)
+            VALUES ($1, $2, $3)
+            RETURNING code, name, description`,
+            [code, name, description]);
+
+        return res.status(201),json({"company": result.rows[0]})
+
     } catch(e) {
         return next(e);
     }
@@ -72,6 +81,21 @@ router.post("/", async (req, res, next) => {
 
 router.put("/:code", async (req, res, next) => {
     try {
+        let {name, description} = req.body;
+        let code = req.params.code;
+
+        const result = await db.query(
+            `UPDATE companies
+            SET name=$1, description=$2
+            WHERE code=$3
+            RETURNING code, name, description`,
+            [name, description, code]);
+        
+        if (result.rows.length === 0) {
+            throw new ExpressError(`No such company: ${code}`, 404)
+        } else {
+            return res.json({"company": result.rows[0]});
+        }
 
     } catch(e) {
         return next(e);
